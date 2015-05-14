95edef01b3d13f2423e385939835580c17ac7dff
Richard Dowson <richard.dowson@gmail.com>
2015-05-14 13:44:29 +0100

    Adds number animation on initial load

--- app/components/numberCell.jsx
+++ app/components/numberCell.jsx
@@ -3,14 +3,28 @@
 var React = require("react");
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {loading: true, value: Math.floor(Math.random() * 89) + 1}
+  },
+  componentDidMount: function() {
+    var timeout =  Math.floor(Math.random() * 1900) + 100;
+    setTimeout(this.finishLoading, timeout);
+  },
+  finishLoading: function(){
+    this.setState({loading: false, value: this.props.value});
+  },
   render: function() {
-    if (this.props.value === 0){
+    if (this.state.value === 0){
       return (
         <div className="numberCell"></div>
       );
     }
+    var classString = "numberCell";
+    if (this.state.loading){
+      classString += " numberCellLoading";
+    }
     return (
-      <div className="numberCell">{this.props.value}</div>
+      <div className={classString}>{this.state.value}</div>
     );
   }
 });
